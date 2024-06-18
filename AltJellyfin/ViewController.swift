39084f386c73723ecb9361d8c8107dbf8bb99a99
Aleksey Mironov <mirlex1986@gmail.com>
2024-06-18 09:36:08 +0300

    Initial Commit

---
+++ AltJellyfin/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  AltJellyfin
+//
+//  Created by Aleksey Mironov on 18.06.2024.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
